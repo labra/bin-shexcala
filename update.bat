6f2a993dba190605f71f2fc90fc012f0b1b6b895
labra <jelabra@gmail.com>
2015-01-06 08:14:54 +0100

    Updated to version 0.2.0

--- update.bat
+++ update.bat
@@ -1,13 +1,13 @@
 set oldversion=0.1.9
 set version=0.2.0
 
-set source=..\src\shapes\ShExcala\target\universal
+set source=../ShExcala/target/universal
 set dest=binaries
 
-rm %dest%\shexcala-%oldversion%.zip
-rm %dest%\shexcala-%oldversion%.tgz
-cp %source%\shexcala-%version%.zip %dest%\shexcala-%version%.zip
-cp %source%\shexcala-%version%.tgz %dest%\shexcala-%version%.tgz
+rm %dest%/shexcala-%oldversion%.zip
+rm %dest%/shexcala-%oldversion%.tgz
+cp %source%/shexcala-%version%.zip %dest%/shexcala-%version%.zip
+cp %source%/shexcala-%version%.tgz %dest%/shexcala-%version%.tgz
 git add -A
 git commit -m "Updated to version %version%"
 git push
